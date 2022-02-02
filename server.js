71e3f1d4a7d649889779c63ea5bd5d5b5d375902
Chris Thorne <u1400900@umail.utah.edu>
2022-02-01 20:57:17 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const routes = require("./routes/apiRoutes");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = reqiure("express-session");
+const path = require("path");
+const hbs = exphbs.create({});
+const sequelize = require("sequelize");
+
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+// Set Handlebars as the default template engine.
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers"));
+app.use(routes);
+app.use(session);
+app.use(sequelize)
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
