c4cba80bd4cac376845e158bd0e676789cdb46a2
tkcannon <taylorkcannon@gmail.com>
2022-02-03 13:50:04 -0700

    fixed errors preventing server from starting

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const routes = require("./routes/apiRoutes");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const session = reqiure("express-session");
+const session = require("express-session");
 const path = require("path");
 const hbs = exphbs.create({});
 const sequelize = require("sequelize");
-
+const controllers = require("./controllers");
 
 // Sets up the Express App
 const app = express();
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 3001;
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(require("./controllers"));
-app.use(routes);
-app.use(session);
-app.use(sequelize)
+// app.use(controllers);
+// app.use(routes);
+// app.use(session);
+// app.use(sequelize)
 // Starts the server to begin listening
 app.listen(PORT, () => {
   console.log("Server listening on: http://localhost:" + PORT);
