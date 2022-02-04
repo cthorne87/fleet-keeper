e8095d837e04c3139599a7b906924633c4a85903
tkcannon <taylorkcannon@gmail.com>
2022-02-04 10:41:41 -0700

    added basic registration model and routes

---
+++ models/Registration.js
@@ -0,0 +1,46 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Registration extends Model { }
+
+Registration.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        state: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        issued_date: {
+            type: Datatypes.DATEONLY,
+            allowNull: false
+        },
+        expiration_date: {
+            type: Datatypes.DATEONLY,
+            allowNull: false
+        },
+        // placeholder / update when user model is made
+        // user_id: {
+        //     type: Datatypes.INTEGER,
+        //     references: {
+        //         model: 'user',
+        //         key: 'id'
+        //     }
+        // }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'registration'
+    }
+)
+
+module.exports = Registration;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const insuranceRoutes = require('./insurance-routes');
+const registrationRoutes = require('./registration-routes');
+
 
 router.use('/insurance', insuranceRoutes);
+router.use('/registration', registrationRoutes);
+
 
 module.exports = router;
