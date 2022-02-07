ae884d4f73fd73f80833c0ed04aa88ed3806f049
tkcannon <taylorkcannon@gmail.com>
2022-02-07 13:59:56 -0700

    setup associations for insurance and registration

--- models/index.js
+++ models/index.js
@@ -1,5 +1,23 @@
+const User;
+const Vehicle;
 const Insurance = require('./Insurance');
 const Registration = require('./Registration');
+// Relations
 
+Insurance.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Insurance.belongsTo(Vehicle, {
+    foreignKey: 'vehicle_id'
+});
+
+Registration.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Registration.belongsTo(Vehicle, {
+    foreignKey: 'vehicle_id'
+})
 
 module.exports = { Insurance, Registration };
