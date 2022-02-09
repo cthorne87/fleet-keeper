4a33cc17060c4661a5e2ad0d85bee9fceb2baecc
tkcannon <taylorkcannon@gmail.com>
2022-02-08 20:52:30 -0700

    renamed routes to controllers

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api/');
-const dashboardRoutes = require('./dashboard-routes.js')
-
-router.use('/', dashboardRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
