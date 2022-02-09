248f329761166e9a49269158348d35c6f3d754d3
Chris Thorne <u1400900@umail.utah.edu>
2022-02-08 21:07:07 -0700

    vehicle-route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const insuranceRoutes = require('./insurance-routes');
 const registrationRoutes = require('./registration-routes');
-
+const userRoutes = require('./user-routes');
+const vehicleRoutes = require('./vehicle-routes');
 
 router.use('/insurance', insuranceRoutes);
 router.use('/registration', registrationRoutes);
-
+router.use('/user', userRoutes);
+router.use('/vehicle', vehicleRoutes);
 
 module.exports = router;
